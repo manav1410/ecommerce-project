aa4c52f6f8a358c19195e803113903598225b7d2
manav <manav.permanent@gmail.com>
2023-07-02 23:42:00 +0530

    - Setted Up server - Setted up mongoose and created user schema

---
+++ src/v1/db/collections.ts
@@ -0,0 +1,51 @@
+import { model, Schema } from "mongoose";
+
+export const Users = model("users", new Schema({
+    fnm: String,
+    lnm: String,
+    mob: {
+        type: String,
+        validate: {
+            validator: (v: string) => /\d{10}/.test(v),
+            message: (props: any) => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    },
+    pass: String,
+    gen: {
+        type: String,
+        enum: { values: ['M', 'F', 'O'], message: '{VALUE} is not supported' }
+    },
+    addr: [new Schema({
+        lt: Number,
+        lg: Number, 
+        ln1: String,
+        ln2: String,
+        ct: String,
+        st: String,
+        zip: {
+            validator: (v: string) => /\d{6}/.test(v),
+            message: (props: any) => `${props.value} is not a valid Pin Code!`
+        },
+    })]
+}));
+
+export const Products = model("products", new Schema({
+    name: String
+}));
+
+export const Orders = model("orders", new Schema({
+
+}));
+
+export const Cart = model("cart", new Schema({
+
+}));
+
+export const OtpRequests = model("otp_requests", new Schema({
+
+}));
+
+export const UserProductEngagement = model("user_product_engagement", new Schema({
+
+}));
